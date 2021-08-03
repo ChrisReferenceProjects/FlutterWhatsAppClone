b43acb5a90d04a6f8b24d0a8e201ab62bd0bd3d3
Christophe Havard <havard.christophe@gmail.com>
2021-08-03 16:07:39 +0200

    fixed kotlin function

--- android/app/src/main/kotlin/com/example/flutterwhatsapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterwhatsapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.flutterwhatsapp
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    private void DoNothing(){
+    fun doNothing() {
         if (b == 0) {  // Noncompliant
             doOneMoreThing()
         } else {
