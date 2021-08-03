4c7eb86a3a47bf54fbb161b9d9051c2cebdb668c
Christophe Havard <havard.christophe@gmail.com>
2021-08-03 16:03:26 +0200

    au pif

--- android/app/src/main/kotlin/com/example/flutterwhatsapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterwhatsapp/MainActivity.kt
@@ -3,4 +3,11 @@ package com.example.flutterwhatsapp
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    private void DoNothing(){
+        if (b == 0) {  // Noncompliant
+            doOneMoreThing()
+        } else {
+            doOneMoreThing()
+        }
+    }
 }
